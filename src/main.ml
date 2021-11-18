20ddbb9633ce9273ef6bf6e7e0381ddaef11b0fc
Son Ho <hosonmarc@gmail.com>
2021-11-18 14:57:01 +0100

    Cleanup more

--- src/main.ml
+++ src/main.ml
@@ -1,18 +1,4 @@
-open Types
-open CfimAst
 open CfimOfJson
-
-type declaration =
-  | Type of TypeDefId.id
-  | Fun of FunDefId.id
-  | RecTypes of TypeDefId.id list
-  | RecFuns of FunDefId.id list
-
-type rust_module = {
-  declarations : declaration list;
-  types : type_def TypeDefId.vector;
-  functions : fun_def FunDefId.vector;
-}
 
 let () =
   let json = Yojson.Basic.from_file "../charon/charon/tests/test1.cfim" in
