ae04cde7b7d5acaf0ae7957276e026a5fcd6706e
Son Ho <hosonmarc@gmail.com>
2022-04-29 18:11:52 +0200

    Add ConstStrings.ml

---
+++ src/ConstStrings.ml
@@ -0,0 +1,7 @@
+(** Some utilities *)
+
+(** Basename for state variables (introduced when using state-error monads) *)
+let state_basename = "st"
+
+(** ADT constructor prefix (used when pretty-printing) *)
+let constructor_prefix = "Mk"
