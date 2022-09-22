ba68c1ab4a7bd7817068d34d44fca38e4c547d90
Son HO <hosonmarc@gmail.com>
2022-09-22 16:25:47 +0200

    Update src/Translate.ml

--- src/Translate.ml
+++ src/Translate.ml
@@ -655,7 +655,7 @@ let translate_module (filename : string) (dest_dir : string) (config : config)
             m.declarations))
   in
 
-  (* Register unique names for all the top-level types, functions and globals.
+  (* Register unique names for all the top-level types, globals and functions.
    * Note that the order in which we generate the names doesn't matter:
    * we just need to generate a mapping from identifier to name, and make
    * sure there are no name clashes. *)
