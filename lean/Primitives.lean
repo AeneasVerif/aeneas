f45eb21b7341ddcaa11e86fcbfc0460c77c33e21
Aymeric Fromherz <aymeric.fromherz@inria.fr>
2022-09-29 18:53:25 +0200

    WIP: Start a stdlib for uints

---
+++ lean/Primitives.lean
@@ -0,0 +1,84 @@
+inductive result (α : Type) where
+  | ret : α -> result α
+  | fail : result α 
+
+open result
+
+def bind (x: result α) (f: α -> result β) : result β :=
+  match x with
+  | ret v  => f v 
+  | fail => fail 
+
+instance : Bind result where
+  bind := bind
+
+def massert (b:Bool) : result Unit :=
+  if b then ret () else fail
+
+def isize_min : Int := -9223372036854775808
+def isize_max : Int := 9223372036854775807
+def i8_min : Int := -128
+def i8_max : Int := 127
+def i16_min : Int := -32768
+def i16_max : Int := 32767
+def i32_min : Int := -2147483648
+def i32_max : Int := 2147483647
+def i64_min : Int := -9223372036854775808
+def i64_max : Int := 9223372036854775807
+def i128_min : Int := -170141183460469231731687303715884105728
+def i128_max : Int := 170141183460469231731687303715884105727
+def usize_min : Int := 0
+def usize_max : Int := 4294967295 -- being conservative here: [u32_max] instead of [u64_max]
+def u8_min : Int := 0
+def u8_max : Int := 255
+def u16_min : Int := 0
+def u16_max : Int := 65535
+def u32_min : Int := 0
+def u32_max : Int := 4294967295
+def u64_min : Int := 0
+def u64_max : Int := 18446744073709551615
+def u128_min : Int := 0
+def u128_max : Int := 340282366920938463463374607431768211455
+
+inductive scalar_ty where
+  | I32
+  | U32
+
+open scalar_ty
+
+def scalar_min (ty:scalar_ty) : Int :=
+  match ty with
+  | I32 => i32_min
+  | U32 => u32_min
+
+def scalar_max (ty:scalar_ty) : Int :=
+  match ty with
+  | I32 => i32_max
+  | U32 => u32_max
+
+/-- TODO: Add refinement on bounds for scalars -/
+def scalar (_ty:scalar_ty) : Type := Int
+
+instance : Coe (scalar ty) Int where
+  coe x := x
+
+instance : OfNat (scalar ty) n where
+  ofNat := Int.ofNat n
+
+def mk_scalar (ty: scalar_ty) (x: Int) : result (scalar ty) :=
+  if scalar_min ty <= x && scalar_max ty >= x then ret x else fail
+
+def scalar_add (x : scalar ty) (y : scalar ty) : result (scalar ty) :=
+  mk_scalar ty (Int.add x y)
+
+/-- Some tests -/
+
+def choose_fwd (b:Bool) (x : t) (y : t) : result t :=
+  if b then ret x else ret y
+
+def choose_bck (b:Bool) (x : t) (y : t) (z : t) : result (t × t) :=
+  if b then ret (z, y) else ret (x, z)
+
+def choose_test : result Unit := do
+  let _x ← choose_fwd true 0 0
+  ret ()
