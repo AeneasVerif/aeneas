7a5fa1744417b8276ea1d287eb9f66789a299863
Son Ho <hosonmarc@gmail.com>
2022-11-13 23:02:00 +0100

    Make minor modifications

--- compiler/ExtractToCoq.ml
+++
@@ -1,8 +0,0 @@
-(** Utilities for the extraction to Coq *)
-
-open Utils
-open Pure
-open TranslateCore
-open ExtractBase
-open StringUtils
-module F = Format
--- compiler/ExtractToFStar.ml
+++
@@ -1,8 +0,0 @@
-(** Utilities for the extraction to F* *)
-
-open Utils
-open Pure
-open TranslateCore
-open ExtractBase
-open StringUtils
-module F = Format
