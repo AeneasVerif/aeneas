5662b755d1281bc2288c8258329827e0b8f9ce82
Jonathan Protzenko <protz@microsoft.com>
2023-01-31 11:11:51 -0800

    Fill out Primitives.lean

--- tests/lean/hashmap_on_disk/lakefile.lean
+++ tests/lean/hashmap_on_disk/lakefile.lean
@@ -1,6 +1,9 @@
 import Lake
 open Lake DSL
 
+require mathlib from git
+  "https://github.com/leanprover-community/mathlib4.git"
+
 package «hashmap» {
   -- add package configuration options here
 }
