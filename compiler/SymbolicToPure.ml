73b95970460fb7d8c90e2ea97fa6457d7850af07
Son Ho <hosonmarc@gmail.com>
2023-03-07 08:51:22 +0100

    Add more checks in Driver.ml

--- compiler/SymbolicToPure.ml
+++ compiler/SymbolicToPure.ml
@@ -2616,7 +2616,8 @@ let wrap_in_match_fuel (fuel0 : VarId.id) (fuel : VarId.id) (body : texpression)
       let match_e = Switch (fuel0, Match [ fail_branch; success_branch ]) in
       { e = match_e; ty = match_ty }
   | Lean ->
-      failwith "Not handling fuel in Lean"
+      (* We should have checked the command line arguments before *)
+      raise (Failure "Unexpected")
 
 let translate_fun_decl (ctx : bs_ctx) (body : S.expression option) : fun_decl =
   (* Translate *)
