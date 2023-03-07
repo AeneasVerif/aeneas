8c8e5affb7b7e92db87e06921516053da3279932
Son Ho <hosonmarc@gmail.com>
2023-03-07 12:31:46 +0100

    Generate all the lakefile.lean files

---
+++ tests/lean/misc/loops/lakefile.lean
@@ -0,0 +1,18 @@
+import Lake
+open Lake DSL
+
+require mathlib from git
+  "https://github.com/leanprover-community/mathlib4.git"
+
+package «loops» {
+  -- add package configuration options here
+}
+
+lean_lib «Base» {
+  -- add library configuration options here
+}
+
+lean_lib «Loops» {
+  -- add library configuration options here
+}
+
