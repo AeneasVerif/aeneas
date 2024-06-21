d3f52e23ede39c4fd7845b6c5feb29d28b2a2384
Nadrieril <nadrieril+git@gmail.com>
2024-06-21 15:47:56 +0200

    Update charon

--- compiler/Interpreter.ml
+++ compiler/Interpreter.ml
@@ -17,7 +17,7 @@ module SA = SymbolicAst
 let log = Logging.interpreter_log
 
 let compute_contexts (m : crate) : decls_ctx =
-  let type_decls_list, _, _, _, _ = split_declarations m.declarations in
+  let type_decls_list, _, _, _, _, _ = split_declarations m.declarations in
   let type_decls = m.type_decls in
   let fun_decls = m.fun_decls in
   let global_decls = m.global_decls in
