f7e3934022d15aded25ff97944c5276b84991cea
Son Ho <hosonmarc@gmail.com>
2025-04-07 17:58:34 +0100

    Fix an issue with the naming of methods in blanket impls

--- tests/coq/misc/BlanketImpl.v
+++ tests/coq/misc/BlanketImpl.v
@@ -15,14 +15,31 @@ Record Trait1_t (Self : Type) := mkTrait1_t{}.
 Arguments mkTrait1_t { _ }.
 
 (** Trait declaration: [blanket_impl::Trait2]
-    Source: 'tests/src/blanket_impl.rs', lines 4:0-4:15 *)
-Record Trait2_t (Self : Type) := mkTrait2_t{}.
+    Source: 'tests/src/blanket_impl.rs', lines 4:0-6:1 *)
+Record Trait2_t (Self : Type) := mkTrait2_t { Trait2_t_foo : result unit; }.
 
 Arguments mkTrait2_t { _ }.
+Arguments Trait2_t_foo { _ } _.
+
+(** [blanket_impl::{blanket_impl::Trait2 for T}::foo]:
+    Source: 'tests/src/blanket_impl.rs', lines 5:4-5:15 *)
+Definition trait2_Blanket_foo
+  {T : Type} (trait1Inst : Trait1_t T) : result unit :=
+  Ok tt
+.
 
 (** Trait implementation: [blanket_impl::{blanket_impl::Trait2 for T}]
-    Source: 'tests/src/blanket_impl.rs', lines 7:0-7:31 *)
+    Source: 'tests/src/blanket_impl.rs', lines 9:0-9:31 *)
 Definition Trait2_Blanket {T : Type} (trait1Inst : Trait1_t T) : Trait2_t T
-  := mkTrait2_t.
+  := {|
+  Trait2_t_foo := trait2_Blanket_foo trait1Inst;
+|}.
+
+(** [blanket_impl::Trait2::foo]:
+    Source: 'tests/src/blanket_impl.rs', lines 5:4-5:15 *)
+Definition trait2_foo_default
+  {Self : Type} (self_clause : Trait2_t Self) : result unit :=
+  Ok tt
+.
 
 End BlanketImpl.
