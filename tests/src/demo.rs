25a5eb07d07b7cfd682947e6e33e614427a9fbf7
Son Ho <hosonmarc@gmail.com>
2025-05-07 19:13:38 -0400

    Reformat the test files

--- tests/src/demo.rs
+++ tests/src/demo.rs
@@ -115,8 +115,8 @@ pub fn use_counter<'a, T: Counter>(cnt: &'a mut T) -> usize {
 
 /* Cryptographic example */
 fn mod_add(a: u32, b: u32) -> u32 {
-    assert!( a < 3329 );
-    assert!( b < 3329 );
+    assert!(a < 3329);
+    assert!(b < 3329);
     let sum = a + b; // 0 <= a + b <= 2 * 3329
     let res = sum.wrapping_sub(3329);
     let mask = res >> 16; // mask = 0xffff if a + b < 3329, mask = 0 otherwise
