0b5b09d84e39bbfbb587fb3cf8792ceee03ccec6
Son Ho <hosonmarc@gmail.com>
2025-05-28 12:55:14 +0100

    Regenerate the tests/lean/lakefile.lean

--- tests/lean/lakefile.lean
+++ tests/lean/lakefile.lean
@@ -17,6 +17,7 @@ package «tests» {}
 @[default_target] lean_lib BlanketImpl
 @[default_target] lean_lib Builtin
 @[default_target] lean_lib Constants
+@[default_target] lean_lib Default
 @[default_target] lean_lib DefaultedMethod
 @[default_target] lean_lib Demo
 @[default_target] lean_lib Deref
