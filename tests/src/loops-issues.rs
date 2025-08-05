1cf7df5a56dc64523f8e461fcdc9593a0c720095
Son Ho <sonho@Sons-MacBook-Pro-2.local>
2025-08-05 22:18:07 +0100

    Reformat the code

--- tests/src/loops-issues.rs
+++ tests/src/loops-issues.rs
@@ -2,10 +2,10 @@
 //! Exercise the translation of loops on examples which proved to be problematic.
 
 /// Dummy helper
-fn write(_ : &mut [u8; 4]) {}
+fn write(_: &mut [u8; 4]) {}
 
 /// Dummy helper
-fn read(_ : &[u8; 4]) {}
+fn read(_: &[u8; 4]) {}
 
 const CARRAY: [u16; 4] = [0, 0, 0, 0];
 
@@ -19,28 +19,23 @@ fn loop_access_array(k: usize) {
 }
 
 /// This comes from an issue found in SymCrust and minimized
-fn loop_array_len()
-{
+fn loop_array_len() {
     let buf = [0u8; 4];
     let _: usize = buf.len();
 
-    loop
-    {
+    loop {
         let _ = buf.len();
     }
 }
 
 /// This comes from an issue found in SymCrust and minimized
-fn loop_array_len_write(b : bool)
-{
+fn loop_array_len_write(b: bool) {
     let mut buf = [0u8; 4];
     let _: usize = buf.len();
 
-    loop
-    {
-        if b
-        {
-            write (&mut buf);
+    loop {
+        if b {
+            write(&mut buf);
         }
     }
 }
@@ -48,37 +43,29 @@ fn loop_array_len_write(b : bool)
 const MAX_NROWS: usize = 4;
 
 /// This comes from an issue found in SymCrust and minimized
-fn read_global_loop(n_rows: usize)
-{
+fn read_global_loop(n_rows: usize) {
     debug_assert!(n_rows <= MAX_NROWS);
     loop {}
 }
 
-fn mut_loop_len(_: &mut u32)
-{
+fn mut_loop_len(_: &mut u32) {
     let buf = [0u8; 4];
 
-    loop
-    {
+    loop {
         debug_assert!(0 <= buf.len());
     }
 }
 
 /// This comes from an issue found in SymCrust and minimized
-fn test(
-    b:bool)
-{
+fn test(b: bool) {
     let mut buf = [0u8; 4];
     let _ = buf.len();
 
-    loop
-    {
-        if b
-        {
+    loop {
+        if b {
             write(&mut buf);
         }
 
         let _ = read(&buf);
     }
 }
-
