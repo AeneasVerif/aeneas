d589173be3830a4a17207dc831a972070167ea03
Nadrieril <nadrieril+git@gmail.com>
2025-09-16 14:45:19 +0200

    Update charon

--- src/symbolic/SymbolicToPureTypes.ml
+++ src/symbolic/SymbolicToPureTypes.ml
@@ -75,6 +75,9 @@ and translate_trait_instance_id (span : Meta.span option)
   | ParentClause (tref, clause_id) ->
       let inst_id = translate_trait_instance_id tref.trait_id in
       ParentClause (inst_id, tref.trait_decl_ref.binder_value.id, clause_id)
+  | ItemClause _ ->
+      (* `ItemClause`s are removed by Charon's `--remove-associated-types`, except for GATs *)
+      [%craise_opt_span] span "Generic Associated Types are not supported yet"
   | Dyn -> [%craise_opt_span] span "Dynamic trait types are not supported yet"
   | UnknownTrait s -> [%craise_opt_span] span ("Unknown trait found: " ^ s)
 
