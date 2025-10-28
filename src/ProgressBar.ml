e75483fe780f89b9e6628dfcf938cf5c5665e7b0
Son Ho <hosonmarc@gmail.com>
2025-10-28 15:30:57 +0000

    Add a progress bar

---
+++ src/ProgressBar.ml
@@ -0,0 +1,6 @@
+let with_reporter (total : int) (msg : string) (f : (int -> unit) -> 'a) : 'a =
+  let bar =
+    let open Progress.Line in
+    list [ const msg; bar total; count_to total; spinner () ]
+  in
+  Progress.with_reporter bar f
