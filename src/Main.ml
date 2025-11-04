4d95ca565a38325763227eba8377e3a223a80911
Son Ho <hosonmarc@gmail.com>
2025-11-04 18:56:04 +0000

    Add an option to deactivate parallel execution

--- src/Main.ml
+++ src/Main.ml
@@ -169,6 +169,9 @@ let () =
          2 with '-mark-ids s1,s2', or '-mark-ids s@1, s@2. The supported \
          prefixes are: 's' (symbolic value id), 'b' (borrow id), 'a' \
          (abstraction id), 'r' (region id), 'f' (pure free variable id)." );
+      ( "-sequential",
+        Arg.Clear parallel,
+        " Execute sequentially (no parallelism)" );
     ]
   in
 
