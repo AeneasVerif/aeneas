869a2dadc3b6b57e3416d9131d83df37c429e185
Son Ho <hosonmarc@gmail.com>
2025-11-24 17:51:50 +0000

    Simplify eval_non_builtin_function_call_symbolic_inst

--- src/interp/InterpStatements.ml
+++ src/interp/InterpStatements.ml
@@ -635,11 +635,11 @@ let eval_non_builtin_function_call_symbolic_inst (span : Meta.span)
             instantiate_fun_sig span ctx func.generics tr_self def.signature
               regions_hierarchy
           in
-          (func.kind, func.generics, None, def, inst_sg)
+          (func.kind, func.generics, def, inst_sg)
       | FunId (FBuiltin _) ->
           (* Unreachable: must be a transparent function *)
           [%craise] span "Unreachable"
-      | TraitMethod (trait_ref, method_name, _) -> (
+      | TraitMethod (trait_ref, method_name, _) ->
           [%ltrace
             "trait method call:\n- call: " ^ call_to_string ctx call
             ^ "\n- method name: " ^ method_name ^ "\n- call.generics:\n"
@@ -652,71 +652,38 @@ let eval_non_builtin_function_call_symbolic_inst (span : Meta.span)
             (trait_ref.trait_decl_ref.binder_regions = [])
             "Unexpected bound regions";
           let trait_decl_ref = trait_ref.trait_decl_ref.binder_value in
-          (* Lookup the trait method signature - there are several possibilities
-             depending on whethere we call a top-level trait method impl or the
-             method from a local clause *)
-          match trait_ref.kind with
-          | TraitImpl { id = impl_id; generics = impl_generics } -> begin
-              (* Lookup the trait impl *)
-              let trait_impl = ctx_lookup_trait_impl span ctx impl_id in
-              [%ltrace "trait impl: " ^ trait_impl_to_string ctx trait_impl];
-              (* Lookup the method *)
-              let fn_ref =
-                Option.get
-                  (Substitute.lookup_and_subst_trait_impl_method trait_impl
-                     method_name impl_generics func.generics)
-              in
-              let method_id = fn_ref.id in
-              let generics = fn_ref.generics in
-              let method_def = ctx_lookup_fun_decl span ctx method_id in
-              (* Instantiate *)
-              let tr_self = trait_ref.kind in
-              let fid : fun_id = FRegular method_id in
-              let regions_hierarchy =
-                LlbcAstUtils.FunIdMap.find fid ctx.fun_ctx.regions_hierarchies
-              in
-              let inst_sg =
-                instantiate_fun_sig span ctx generics tr_self
-                  method_def.signature regions_hierarchy
-              in
-              (* Also update the function identifier: we want to forget
-                 the fact that we called a trait method, and treat it as
-                 a regular function call to the top-level function
-                 which implements the method. In order to do this properly,
-                 we also need to update the generics.
-              *)
-              let func = FunId fid in
-              (* TODO: the `trait_method_generics` look fishy *)
-              (func, generics, method_def, inst_sg)
-            end
-          | _ ->
-              (* We are using a local clause - we lookup the trait decl *)
-              let trait_decl =
-                ctx_lookup_trait_decl span ctx trait_decl_ref.id
-              in
-              (* Lookup the method decl in the required *and* the provided methods *)
-              let fn_ref =
-                Option.get
-                  (Substitute.lookup_and_subst_trait_decl_method trait_decl
-                     method_name trait_ref func.generics)
-              in
-              let method_id = fn_ref.id in
-              let generics = fn_ref.generics in
-              let method_def = ctx_lookup_fun_decl span ctx method_id in
-              [%ltrace "method:\n" ^ fun_decl_to_string ctx method_def];
-              (* Instantiate *)
-              (* When instantiating, we need to group the generics for the
+          (* This should be a call to a trait clause method (if it were a method
+             coming from an impl, there should be no indirection through the trait
+             reference itself (Charon should have simplified this). *)
+          [%sanity_check] span
+            (match trait_ref.kind with
+            | TraitImpl _ -> false
+            | _ -> true);
+          (* Lookup the trait decl *)
+          let trait_decl = ctx_lookup_trait_decl span ctx trait_decl_ref.id in
+          (* Lookup the method decl in the required *and* the provided methods *)
+          let fn_ref =
+            Option.get
+              (Substitute.lookup_and_subst_trait_decl_method trait_decl
+                 method_name trait_ref func.generics)
+          in
+          let method_id = fn_ref.id in
+          let generics = fn_ref.generics in
+          let method_def = ctx_lookup_fun_decl span ctx method_id in
+          [%ltrace "method:\n" ^ fun_decl_to_string ctx method_def];
+          (* Instantiate *)
+          (* When instantiating, we need to group the generics for the
                  trait ref and the generics for the method *)
-              let regions_hierarchy =
-                LlbcAstUtils.FunIdMap.find (FRegular method_id)
-                  ctx.fun_ctx.regions_hierarchies
-              in
-              let tr_self = trait_ref.kind in
-              let inst_sg =
-                instantiate_fun_sig span ctx generics tr_self
-                  method_def.signature regions_hierarchy
-              in
-              (func.kind, func.generics, method_def, inst_sg)))
+          let regions_hierarchy =
+            LlbcAstUtils.FunIdMap.find (FRegular method_id)
+              ctx.fun_ctx.regions_hierarchies
+          in
+          let tr_self = trait_ref.kind in
+          let inst_sg =
+            instantiate_fun_sig span ctx generics tr_self method_def.signature
+              regions_hierarchy
+          in
+          (func.kind, func.generics, method_def, inst_sg))
 
 (** Helper: introduce a fresh symbolic value for a global *)
 let eval_global_as_fresh_symbolic_value (span : Meta.span)
