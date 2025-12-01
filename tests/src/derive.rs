9ee3e5f4bcb28839647db21f83a9a02602441571
Son Ho <hosonmarc@gmail.com>
2025-12-01 18:29:36 +0000

    Add tests/src/derive.rs

--- tests/src/derive.rs
+++ tests/src/derive.rs
@@ -1,7 +1,23 @@
 //@ [!lean] skip
 
-// TODO: #[derive(PartialEq, Eq, Clone, Debug)]
-#[derive(Clone)]
+// TODO: #[derive(Debug)]
+
+enum ScalarEnum {
+    Variant0 = 2,
+    Variant1 = 4,
+    Variant2 = 8,
+    Variant3 = 16,
+}
+
+#[derive(Clone, Copy, PartialEq, Eq)]
+enum CopyEnum<T> {
+    Variant0,
+    Variant1(bool),
+    Variant2(u32),
+    Variant3(T),
+}
+
+#[derive(Clone, PartialEq, Eq)]
 enum Enum<T> {
     Variant0,
     Variant1(bool),
@@ -10,18 +26,22 @@ enum Enum<T> {
     Variant4(Vec<T>),
 }
 
-#[derive(Clone)]
+#[derive(Clone, PartialEq, Eq)]
 enum List<T> {
     Nil,
     Cons(T, Box<List<T>>),
     // TODO: Vec<...>
 }
 
-#[derive(Clone)]
-struct Struct<T> {
+#[derive(Clone, Copy, PartialEq, Eq)]
+struct CopyStruct<T> {
     f0 : (),
     f1: bool,
     f2: u32,
     f3: T,
-    f4: Vec<T>,
+}
+
+#[derive(Clone, PartialEq, Eq)]
+struct Struct<T> {
+    f: Vec<T>,
 }
