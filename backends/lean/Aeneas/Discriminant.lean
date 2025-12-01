a26dca7711e81a91b27d2ba2ad2556cad12b0c82
Son Ho <hosonmarc@gmail.com>
2025-12-01 16:17:37 +0000

    Add the Lean typeclass `Discriminant` and derive it with `@[discriminant]`

---
+++ backends/lean/Aeneas/Discriminant.lean
@@ -0,0 +1,164 @@
+import Lean
+import Aeneas.Std.Scalar.Core
+import Aeneas.Std.Scalar.Notations
+
+namespace Aeneas
+
+open Lean Elab Term Meta
+open Lean.Parser.Term Command
+
+namespace Std
+
+/-- This typeclass models the discriminant reads which sometimes happen in Rust -/
+class Discriminant (α : Type u) where
+  read_discriminant : α → Std.Isize
+
+export Discriminant (read_discriminant)
+
+end Std
+
+namespace Discriminant
+
+initialize registerTraceClass `Discriminant
+
+/-- Auxiliary helper for `generateReadDiscriminant`.
+
+This function is adapted from `Lean.Elab.Deriving.BEq`.
+-/
+def generateReadDiscriminantCmds (declName : Name) (discrValues : Option (List Nat)) :
+  TermElabM (List Syntax) := do
+  -- Lookup the declaration, which should be an inductive
+  let env ← getEnv
+  let some decl := env.findAsync? declName
+    | throwError "Could not find declaration {declName}"
+  let indVal ← match decl.constInfo.get with
+    | .inductInfo const => pure const
+    | _ => throwError "Declaration is not an inductive: {declName}"
+
+  /- We try to piggyback as much as possible on the utilities introducing for `deriving`.
+  In particular, the following helper generates the parameter names and binders we need.
+
+  The only caveat is that it introduces typeclass instances for the generic types.
+  For instance, if we give it type `inductive Foo (α β : Type)` it will generate binders:
+  `{α β : Type} [Discriminant α] [Discriminant β] (x : Foo α β)`
+  Because of this, we need to update the binders to drop `[Discriminant α]` and `[Discriminant β]`
+  (TODO: this is rather inelegant).
+  -/
+  let header ← Lean.Elab.Deriving.mkHeader ``Std.Discriminant 1 indVal
+  trace[Discriminant] "numParams: {indVal.numParams}"
+  let header : Deriving.Header :=
+    let binders := header.binders
+    let binders := binders.extract 0 indVal.numParams ++ [binders[binders.size - 1]!]
+    { header with binders }
+
+  -- Generate the value of the discriminant for each variant
+  let discrValues ← do
+    match discrValues with
+    | none => pure (indVal.ctors.mapIdx (fun n _ => n))
+    | some values =>
+      if values.length ≠ indVal.ctors.length then
+        throwError "Invalid number of values provided ({discrValues}): got {values.length}, expected {indVal.ctors.length}"
+      pure values
+
+  -- Generate the match branches
+  let alts : List (TSyntax `Lean.Parser.Term.matchAltExpr) ←
+    indVal.ctors.mapIdxM fun i ctorName => do
+    let ctorInfo ← getConstInfoCtor ctorName
+    let value := Syntax.mkNumLit (toString discrValues[i]!)
+    let alt ← do
+      -- Generate one `_` pattern for each index then for each field
+      let mut patterns := #[]
+      for _ in 0...(indVal.numIndices + ctorInfo.numFields) do
+        patterns := patterns.push (← `(_))
+      let pat ← `($(mkIdent ctorName):ident $patterns:term*)
+      `(matchAltExpr| | $pat => $(⟨value⟩)#isize)
+    pure alt
+  let alts := alts.toArray
+
+  -- Generate the match itself
+  let varName := header.targetNames[0]!
+  let discr : TSyntax ``Parser.Term.matchDiscr ←
+    `(Parser.Term.matchDiscr| $(mkIdent varName):term)
+  let discrs : Array (TSyntax `Lean.Parser.Term.matchDiscr) := #[discr]
+  let body ← `(match $[$discrs],* with $alts:matchAlt*)
+
+  -- Generate the syntax for function definition
+  let auxFunName := Name.mkStr declName "read_discriminant"
+  let binders := header.binders
+  let defStx ← `(def $(mkIdent auxFunName):ident $binders:bracketedBinder* : Aeneas.Std.Isize := $body:term)
+
+  -- Generate the syntax for the instance
+  let binders := binders.extract 0 indVal.numParams
+  let args := header.argNames.map mkIdent
+  let instStx ← `(instance $binders:bracketedBinder* : Aeneas.Std.Discriminant ($header.targetType) where
+      read_discriminant := @$(mkIdent auxFunName):ident $args*)
+
+  --
+  pure [defStx, instStx]
+
+/-- Given an inductive declaration name and an optional list of values, generate an instance
+of `Std.Discriminant`. If the list of values is not provided, we use values `0`, `1`, etc. -/
+def generateReadDiscriminant (declName : Name) (discrValues : Option (List Nat)) :
+  CommandElabM Unit := do
+  let cmds ← liftTermElabM (generateReadDiscriminantCmds declName discrValues)
+  cmds.forM elabCommand
+
+syntax (name := readDiscriminant) "discriminant" ("["num,*"]")? : attr
+
+def elabReadDiscriminantAttribute (stx : Syntax) : AttrM (Option (List Nat)) :=
+  withRef stx do
+    match stx with
+    | `(attr| discriminant) => do pure none
+    | `(attr| discriminant [$x,*]) => do pure (some ((x.getElems.toList.map Syntax.isNatLit?).map Option.get!))
+    | _ => throwUnsupportedSyntax
+
+initialize discriminantAttribute : AttributeImpl ← do
+  let attrImpl : AttributeImpl := {
+    name := `readDiscriminant
+    descr := "Generates an instance of `Std.Discriminant` for the given inductive"
+    add := fun declName stx attrKind => do
+      -- Elaborate the attribute
+      let values ← elabReadDiscriminantAttribute stx
+      -- Generate the definitions
+      liftCommandElabM (generateReadDiscriminant declName values)
+  }
+  registerBuiltinAttribute attrImpl
+  pure attrImpl
+
+namespace Test
+
+  open Std
+
+  mutual
+
+  inductive Foo (α β : Type) where
+  | Bar (y : Nat)
+  | Baz (x : α) (y : β)
+
+  inductive Foo1 (α β : Type) where
+  | Variant1 | Variant2
+
+  end
+
+  inductive Foo2 where
+  | Variant1
+  | Variant2
+
+  inductive Foo3 where
+  | Variant1
+  | Variant2
+
+  #eval generateReadDiscriminant ``Foo (some [3, 4])
+  #eval generateReadDiscriminant ``Foo1 none
+  #eval generateReadDiscriminant ``Foo2 none
+  #eval generateReadDiscriminant ``Foo3 (some [3, 4])
+
+  #assert read_discriminant Foo2.Variant1 = 0#isize
+  #assert read_discriminant Foo3.Variant1 = 3#isize
+  #assert read_discriminant Foo3.Variant2 = 4#isize
+
+end Test
+
+end Discriminant
+
+end Aeneas
