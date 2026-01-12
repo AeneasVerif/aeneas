b3f248a055eb24d425f45c478c91bcc9e632b196
Son Ho <hosonmarc@gmail.com>
2026-01-12 16:33:44 +0000

    Add models and extend a test

--- tests/src/into.rs
+++ tests/src/into.rs
@@ -4,3 +4,7 @@ use std::convert::TryInto;
 fn slice_to_array(s: &[u8]) -> &[u8; 32] {
     s.try_into().unwrap()
 }
+
+fn slice_to_array1(s: &[u8]) -> &[u8; 32] {
+    s.try_into().expect("Expected a slice of length 32")
+}
