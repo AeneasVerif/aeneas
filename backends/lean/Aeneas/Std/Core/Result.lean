e71edcfbec506216168270712743c6a7e6c161b3
Son Ho <hosonmarc@gmail.com>
2026-02-11 20:47:10 +0000

    More some type variables universe polymorphic in the standard library

--- backends/lean/Aeneas/Std/Core/Result.lean
+++ backends/lean/Aeneas/Std/Core/Result.lean
@@ -3,7 +3,7 @@ import Aeneas.Extract
 namespace Aeneas.Std
 
 @[rust_type "core::result::Result"]
-inductive core.result.Result (T : Type) (E : Type) where
+inductive core.result.Result (T : Type u) (E : Type v) where
 | Ok : T → core.result.Result T E
 | Err : E → core.result.Result T E
 
