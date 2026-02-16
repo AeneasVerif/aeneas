e20f8ad010f3357b7ba4cf95e3a21fbcd759eb1f
Son Ho <hosonmarc@gmail.com>
2026-02-16 10:26:02 +0000

    Properly extract the wrapping binops to Lean

--- backends/lean/Aeneas/Std/Scalar/WrappingOps/Mul.lean
+++ backends/lean/Aeneas/Std/Scalar/WrappingOps/Mul.lean
@@ -11,13 +11,19 @@ open Result Error ScalarElab
 
 def UScalar.wrapping_mul {ty} (x y : UScalar ty) : UScalar ty := ⟨ x.bv * y.bv ⟩
 
-/- [core::num::{u8}::wrapping_mul] -/
+def IScalar.wrapping_mul {ty} (x y : IScalar ty) : IScalar ty := ⟨ x.bv * y.bv ⟩
+
+uscalar @[progress_pure_def]
+def «%S».wrapping_mul (x y : «%S») : «%S» := @UScalar.wrapping_mul UScalarTy.«%S» x y
+
+iscalar @[progress_pure_def]
+def «%S».wrapping_mul (x y : «%S») : «%S» := @IScalar.wrapping_mul IScalarTy.«%S» x y
+
+/- [core::num::{_}::wrapping_mul] -/
 uscalar @[progress_pure_def]
 def core.num.«%S».wrapping_mul : «%S» → «%S» → «%S» := @UScalar.wrapping_mul UScalarTy.«%S»
 
-def IScalar.wrapping_mul {ty} (x y : IScalar ty) : IScalar ty := ⟨ x.bv * y.bv ⟩
-
-/- [core::num::{i8}::wrapping_mul] -/
+/- [core::num::{_}::wrapping_mul] -/
 iscalar @[progress_pure_def]
 def core.num.«%S».wrapping_mul : «%S» → «%S» → «%S»  := @IScalar.wrapping_mul IScalarTy.«%S»
 
@@ -26,16 +32,24 @@ def core.num.«%S».wrapping_mul : «%S» → «%S» → «%S»  := @IScalar.wra
   simp only [wrapping_mul]
 
 uscalar @[simp, bvify_simps] theorem «%S».wrapping_mul_bv_eq (x y : «%S») :
+  («%S».wrapping_mul x y).bv = x.bv * y.bv := by
+  simp [«%S».wrapping_mul]
+
+uscalar @[simp, bvify_simps] theorem core.num.«%S».wrapping_mul_bv_eq (x y : «%S») :
   (core.num.«%S».wrapping_mul x y).bv = x.bv * y.bv := by
-  simp [core.num.«%S».wrapping_mul, bv]
+  simp [core.num.«%S».wrapping_mul]
 
 @[simp, bvify_simps] theorem IScalar.wrapping_mul_bv_eq {ty} (x y : IScalar ty) :
   (wrapping_mul x y).bv = x.bv * y.bv := by
   simp only [wrapping_mul]
 
 iscalar @[simp, bvify_simps] theorem «%S».wrapping_mul_bv_eq (x y : «%S») :
+  («%S».wrapping_mul x y).bv = x.bv * y.bv := by
+  simp [«%S».wrapping_mul]
+
+iscalar @[simp, bvify_simps] theorem core.num.«%S».wrapping_mul_bv_eq (x y : «%S») :
   (core.num.«%S».wrapping_mul x y).bv = x.bv * y.bv := by
-  simp [core.num.«%S».wrapping_mul, bv]
+  simp [core.num.«%S».wrapping_mul]
 
 @[simp] theorem UScalar.wrapping_mul_val_eq {ty} (x y : UScalar ty) :
   (wrapping_mul x y).val = (x.val * y.val) % (UScalar.size ty) := by
@@ -45,6 +59,10 @@ iscalar @[simp, bvify_simps] theorem «%S».wrapping_mul_bv_eq (x y : «%S») :
   simp only [BitVec.toNat_mul, bv_toNat]
 
 uscalar @[simp] theorem «%S».wrapping_mul_val_eq (x y : «%S») :
+  («%S».wrapping_mul x y).val = (x.val * y.val) % (UScalar.size .«%S») :=
+  UScalar.wrapping_mul_val_eq x y
+
+uscalar @[simp] theorem core.num.«%S».wrapping_mul_val_eq (x y : «%S») :
   (core.num.«%S».wrapping_mul x y).val = (x.val * y.val) % (UScalar.size .«%S») :=
   UScalar.wrapping_mul_val_eq x y
 
@@ -54,6 +72,10 @@ uscalar @[simp] theorem «%S».wrapping_mul_val_eq (x y : «%S») :
   simp only [BitVec.toInt_mul, bv_toInt_eq]
 
 iscalar @[simp] theorem «%S».wrapping_mul_val_eq (x y : «%S») :
+  («%S».wrapping_mul x y).val = Int.bmod (x.val * y.val) (2^ %BitWidth) :=
+  IScalar.wrapping_mul_val_eq x y
+
+iscalar @[simp] theorem core.num.«%S».wrapping_mul_val_eq (x y : «%S») :
   (core.num.«%S».wrapping_mul x y).val = Int.bmod (x.val * y.val) (2^ %BitWidth) :=
   IScalar.wrapping_mul_val_eq x y
 
