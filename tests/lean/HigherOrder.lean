51b1242c8c600cf126f3e2c1917f22a8f0df0724
Matej Penciak <matej.penciak@gmail.com>
2026-03-25 19:40:18 -0500

    reconsider step*

--- tests/lean/HigherOrder.lean
+++ tests/lean/HigherOrder.lean
@@ -13,10 +13,13 @@ theorem applyF_spec (f : U32 → Result U32) (x : U32)
     applyF f x ⦃ post ⦄ := by
   simp [applyF]; exact hf
 
-set_option trace.Step true in
+set_option trace.Step true
+
 example (a : U32) (h : a.val + 1 ≤ U32.max) :
     applyF (fun x => x + 1#u32) a ⦃ y => y.val = a.val + 1 ⦄ := by
-  step +inferPost
+  step
+  case hf => step +inferPost
+  grind
 
 -- Higher-order in 2 functions, operates on a pair of inputs/outputs
 def callPair (f : U32 → Result U32) (g : U32 → Result U32) (xy : U32 × U32) : Result (U32 × U32) := do
@@ -37,18 +40,12 @@ theorem callPair_spec (f g : U32 → Result U32) (xy : U32 × U32)
   simp [WP.spec, WP.theta, WP.wp_return]
   exact ⟨ha, hb⟩
 
-/- /-- -/
-/- info: Try this: -/
-/--/
-/-   [apply]     let* ⟨ ab, ab_post1, ab_post2 ⟩ ← [ +inferPost ] callPair_spec -/
-/-     case hf => let* ⟨ ⟩ ← [ +inferPost ] U32.add_spec -/
-/-     case hg => let* ⟨ ⟩ ← [ +inferPost ] U32.add_spec -/
-/-     agrind -/
-/- -/ -/
-/- #guard_msgs in -/
 example (x y : U32) (hx : x.val + 1 ≤ U32.max) (hy : y.val + 2 ≤ U32.max) :
     callPair (fun a => a + 1#u32) (fun b => b + 2#u32) (x, y) ⦃ ab => ab.1.val = x.val + 1 ∧ ab.2.val = y.val + 2 ⦄ := by
-  step* +inferPost
+  step
+  case hf => step +inferPost
+  case hg => step +inferPost
+  grind
 
 -- Calls f then g in sequence
 def callFThenG (f g : U32 → Result U32) (x : U32) : Result U32 := do
@@ -66,20 +63,12 @@ theorem callFThenG_spec (f g : U32 → Result U32) (x : U32)
   intro y hy
   exact hg y hy
 
-/--
-info: Try this:
-
-  [apply]     let* ⟨ y, y_post ⟩ ← [ +inferPost ] callFThenG_spec
-    case hf => let* ⟨ ⟩ ← [ +inferPost ] U32.add_spec
-    case hg =>
-      intros
-      let* ⟨ ⟩ ← [ +inferPost ] U32.add_spec
-    agrind
--/
-#guard_msgs in
 example (x : U32) (h1 : x.val + 1 ≤ U32.max) (h2 : x.val + 2 ≤ U32.max) :
     callFThenG (fun a => a + 1#u32) (fun b => b + 1#u32) x ⦃ y => y.val = x.val + 2 ⦄ := by
-  step*? +inferPost
+  step
+  case hf => step +inferPost
+  case hg => intros; step +inferPost
+  grind
 
 def _root_.Aeneas.Std.Slice.mapM  {α β} (f : α → Result β) (x : Slice α) : Result (Slice β) :=
   match h : x.val.mapM f with
@@ -98,22 +87,16 @@ def callSlicemapM (x : Slice U32) : Result (Slice U32) := do
   let y ← x.mapM (fun x => x + 1#u32)
   return y
 
-/--
-info: Try this:
-
-  [apply]     let* ⟨ y, y_post1, y_post2 ⟩ ← [ +inferPost ] Slice.mapM_spec
-    case hf =>
-      intros
-      let* ⟨ ⟩ ← [ +inferPost ] U32.add_spec
-    agrind
--/
-#guard_msgs in
 example (s : Slice U32) (h : ∀ i (hi : i < s.len), s[i] < U32.max) :
   callSlicemapM s ⦃ s' =>
     s'.len = s.len ∧
     ∀ i (hi₁ : i < s.len) (hi₂ : i < s'.len), s'[i].val = s[i].val + 1
     ⦄ := by
   unfold callSlicemapM
-  step*? +inferPost
+  step +inferPost
+  case hf =>
+    intros
+    step +inferPost
+  grind
 
 end higher_order
