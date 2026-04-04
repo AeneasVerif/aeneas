48d3155454d1dcd593a48adfc8e4465b769f563f
Kevin Lotz <lotzk@outlook.com>
2026-04-04 17:01:21 +0200

    feat(lean):  add theorem  UScalar.overflowing_sub_eq

--- backends/lean/Aeneas/Std/Scalar/OverflowingOps.lean
+++ backends/lean/Aeneas/Std/Scalar/OverflowingOps.lean
@@ -107,5 +107,57 @@ uscalar def core.num.«%S».overflowing_sub := @UScalar.overflowing_sub .«%S»
 /- [core::num::{i8}::overflowing_sub] -/
 iscalar def core.num.«%S».overflowing_sub := @IScalar.overflowing_sub .«%S»
 
+theorem UScalar.overflowing_sub_eq {ty} (x y : UScalar ty) :
+  let z := overflowing_sub x y
+  if x.val < y.val then
+    z.fst.val + y.val = x.val + UScalar.size ty ∧
+    z.snd = true
+  else
+    z.fst.val = x.val - y.val ∧
+    z.snd = false
+  := by
+  simp [overflowing_sub]
+  have hsub_toNat : (x.bv - y.bv).toNat =
+      (x.val + (2 ^ ty.numBits - y.val)) % 2 ^ ty.numBits := by
+    simp [BitVec.toNat_sub, bv_toNat, Nat.add_comm]
+  have hsubval : (↑({ bv := x.bv - y.bv } : UScalar ty) : Nat) =
+      (x.val + (2 ^ ty.numBits - y.val)) % 2 ^ ty.numBits := by
+    exact hsub_toNat
+  split
+  case isTrue ht =>
+    apply And.intro
+    · rw [hsubval]
+      have hmod :
+          (x.val + (2 ^ ty.numBits - y.val)) % 2 ^ ty.numBits =
+            x.val + (2 ^ ty.numBits - y.val) := by
+        apply Nat.mod_eq_of_lt
+        grind
+      rw [hmod]
+      rw [UScalar.size]
+      grind
+    · simp [ht]
+  case isFalse hf =>
+    apply And.intro
+    · have hxy : y.val ≤ x.val := Nat.le_of_not_gt hf
+      rw [hsubval]
+      have hsubmod :
+          (x.val + (2 ^ ty.numBits - y.val)) % 2 ^ ty.numBits =
+            (x.val + (2 ^ ty.numBits - y.val) - 2 ^ ty.numBits) % 2 ^ ty.numBits := by
+        rw [Nat.mod_eq_sub_mod]
+        omega
+      rw [hsubmod]
+      have hsub : x.val + (2 ^ ty.numBits - y.val) - 2 ^ ty.numBits = x.val - y.val := by grind
+      rw [hsub]
+      apply Nat.mod_eq_of_lt
+      grind
+    · grind
+
+uscalar @[step_pure overflowing_sub x y]
+theorem core.num.«%S».overflowing_sub_eq (x y : «%S») :
+  let z := overflowing_sub x y
+  if x.val < y.val then z.fst.val + y.val = x.val + UScalar.size .«%S» ∧ z.snd = true
+  else  z.fst.val = x.val - y.val ∧ z.snd = false
+  := UScalar.overflowing_sub_eq x y
+
 
 end Aeneas.Std
