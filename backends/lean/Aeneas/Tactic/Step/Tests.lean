eb0bf377f9852e06228a28dc182012462554f039
Son Ho <hosonmarc@gmail.com>
2026-06-23 07:50:01 +0100

    Improve bind_assoc rewritings

--- backends/lean/Aeneas/Tactic/Step/Tests.lean
+++ backends/lean/Aeneas/Tactic/Step/Tests.lean
@@ -3,3 +3,4 @@ import Aeneas.Tactic.Step.Tests.IntroOutputs
 import Aeneas.Tactic.Step.Tests.MvcgenSpec
 import Aeneas.Tactic.Step.Tests.TupleDestruct
 import Aeneas.Tactic.Step.Tests.UncurryBind
+import Aeneas.Tactic.Step.Tests.BindAssoc
