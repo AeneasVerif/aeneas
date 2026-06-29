5220259c236e10b7abadc086fdd1b2c507f9e6bb
Son HO <hosonmarc@gmail.com>
2026-06-29 11:52:30 +0200

    Add support for boolean comparisons (#1168)

--- tests/lean/lakefile.lean
+++ tests/lean/lakefile.lean
@@ -15,6 +15,7 @@ package «tests» {}
 @[default_target] lean_lib Avl
 @[default_target] lean_lib BaseTutorial
 @[default_target] lean_lib Bitwise
+@[default_target] lean_lib BoolCmp
 @[default_target] lean_lib BlanketImpl
 @[default_target] lean_lib Bst
 @[default_target] lean_lib Builtin
