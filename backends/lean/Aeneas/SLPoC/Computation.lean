01ca03ef38663caf80cf1257557d355c45347586
Cezar Andrici <cezar.andrici@gmail.com>
2026-08-07 16:57:04 +0100

    Rename ref to Ref

--- backends/lean/Aeneas/SLPoC/Computation.lean
+++ backends/lean/Aeneas/SLPoC/Computation.lean
@@ -4,10 +4,10 @@ import Aeneas.SLPoC.Heap
 namespace Aeneas.SLPoC
 
 inductive StEvents : Type → Type 1 where
-  | Alloc {α : Type} (value : α) : StEvents (ref α)
-  | Read {α : Type} (r : ref α) : StEvents α
-  | Update {α : Type} (r : ref α) (value : α) : StEvents Unit
-  | Free {α : Type} (r : ref α) : StEvents Unit
+  | Alloc {α : Type} (value : α) : StEvents (Ref α)
+  | Read {α : Type} (r : Ref α) : StEvents α
+  | Update {α : Type} (r : Ref α) (value : α) : StEvents Unit
+  | Free {α : Type} (r : Ref α) : StEvents Unit
 
 abbrev St := FFree StEvents
 
@@ -16,39 +16,39 @@ instance St.instLawfulMonad : LawfulMonad St :=
 
 namespace State
 
-def alloc {α : Type} (value : α) : St (ref α) :=
+def alloc {α : Type} (value : α) : St (Ref α) :=
   FFree.trigger (.Alloc value)
 
-def read {α : Type} (r : ref α) : St α :=
+def read {α : Type} (r : Ref α) : St α :=
   FFree.trigger (.Read r)
 
-def update {α : Type} (r : ref α) (value : α) : St Unit :=
+def update {α : Type} (r : Ref α) (value : α) : St Unit :=
   FFree.trigger (.Update r value)
 
-def free {α : Type} (r : ref α) : St Unit :=
+def free {α : Type} (r : Ref α) : St Unit :=
   FFree.trigger (.Free r)
 end State
 
 inductive Evaluates : St α → Heap → α → Heap → Prop where
   | ok (value : α) (h : Heap) :
       Evaluates (.ok value) h value h
-  | alloc {β : Type} {value : β} {next : ref β → St α}
-      {h₀ h₁ h₂ : Heap} {r : ref β} {result : α}
+  | alloc {β : Type} {value : β} {next : Ref β → St α}
+      {h₀ h₁ h₂ : Heap} {r : Ref β} {result : α}
       (hFresh : fresh h₀ r value h₁)
       (hNext : Evaluates (next r) h₁ result h₂) :
       Evaluates (.event (.Alloc value) next) h₀ result h₂
-  | read {β : Type} {r : ref β} {next : β → St α}
+  | read {β : Type} {r : Ref β} {next : β → St α}
       {h₀ h₁ : Heap} {result : α}
       (hContains : contains h₀ r)
       (hNext : Evaluates (next (read r h₀ hContains)) h₀ result h₁) :
       Evaluates (.event (.Read r) next) h₀ result h₁
-  | update {β : Type} {r : ref β} {value : β} {next : Unit → St α}
+  | update {β : Type} {r : Ref β} {value : β} {next : Unit → St α}
       {h₀ h₁ : Heap} {result : α}
       (hContains : contains h₀ r)
       (hNext :
         Evaluates (next ()) (update r value h₀ hContains) result h₁) :
       Evaluates (.event (.Update r value) next) h₀ result h₁
-  | free {β : Type} {r : ref β} {next : Unit → St α}
+  | free {β : Type} {r : Ref β} {next : Unit → St α}
       {h₀ h₁ : Heap} {result : α}
       (hContains : contains h₀ r)
       (hNext : Evaluates (next ()) (free r h₀ hContains) result h₁) :
