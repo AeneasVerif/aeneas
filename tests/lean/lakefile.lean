6022804326451799d4ed68d6d0204acfad164683
Son HO <hosonmarc@gmail.com>
2026-09-03 11:57:42 +0100

    Implement PrePasses.fix_closure_signature_regions (#1333)

--- tests/lean/lakefile.lean
+++ tests/lean/lakefile.lean
@@ -48,6 +48,7 @@ package «tests» {}
 @[default_target] lean_lib Issue1138SlicePositivity
 @[default_target] lean_lib Issue1140GlobalLoop
 @[default_target] lean_lib Issue1141ImplFnRef
+@[default_target] lean_lib Issue1207ClosureSignatureRegions
 @[default_target] lean_lib Issue1250AnonymousConst
 @[default_target] lean_lib Issue1260EliminateSharedLoans
 @[default_target] lean_lib Issue134LoopSharedBorrows
