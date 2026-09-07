9935e219a24b33f41c9869701dc3526ae885152c
Cezar Andrici <cezar.andrici@gmail.com>
2026-09-07 15:06:19 +0100

    snap

---
+++ backends/lean/Aeneas/SLPoC/Tests.lean
@@ -0,0 +1,8 @@
+import Aeneas.SLPoC.Tests.Array
+import Aeneas.SLPoC.Tests.Choice
+import Aeneas.SLPoC.Tests.Examples
+import Aeneas.SLPoC.Tests.Partial
+import Aeneas.SLPoC.Tests.PureSpec
+import Aeneas.SLPoC.Tests.Run
+import Aeneas.SLPoC.Tests.Step
+import Aeneas.SLPoC.Tests.UnitTest
