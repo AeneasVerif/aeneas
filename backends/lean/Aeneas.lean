0b26b4f98b1c86a7e8df08ce143540865edff841
Cezar Andrici <cezar.andrici@gmail.com>
2026-09-18 15:10:17 +0100

    snap

--- backends/lean/Aeneas.lean
+++ backends/lean/Aeneas.lean
@@ -4,7 +4,6 @@ public import Aeneas.Data
 public import Aeneas.Do
 public import Aeneas.Extract
 public import Aeneas.SepLogic
-public import Aeneas.SepLogic.Semantics
 public import Aeneas.Std
 -- Re-export `Aeneas.Std` as `meta` since `#assert` often needs it.
 public meta import Aeneas.Std
