2e57a0937662f9c6d4ac012d25bd27b7e4a3ab3b
Camilo Tello Fachin <101008039+tellocam@users.noreply.github.com>
2026-09-25 15:28:51 +0200

    Update tests/src/order.rs

--- tests/src/order.rs
+++ tests/src/order.rs
@@ -135,7 +135,7 @@ pub fn test_num_incomparable() {
 }
 
 // ---------------------------------------------------------------------------
-// exercises impl PartialOrd<&B> for &A, its how the issues own example examle (a < b on two &Foo) compiles.
+// exercises impl PartialOrd<&B> for &A, it's how the issue's own example (a < b on two &Foo) compiles.
 // ---------------------------------------------------------------------------
 
 #[verify::test]
